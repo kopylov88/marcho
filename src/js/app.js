f8ec8579c9575b067645a09fe9a42f2281b73389
kopylov88 <kopyl.88@mail.ru>
2023-01-09 13:34:24 +0300

    added top-slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
-import * as myFunctions from "./modules/functions.js";
+import * as myFunctions from './modules/functions.js';
+import $ from 'jquery';
+import 'slick-carousel';
 
 myFunctions.isWebp();
+$('.top-slider__inner').slick({
+  dots: true,
+  arrows: false,
+  fade: true,
+  autoplay: true,
+});
