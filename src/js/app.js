f82a65aaee96b147b84d782caa03c13ca143986c
kopylov88 <kopyl.88@mail.ru>
2023-01-11 16:59:50 +0300

    added tranding fashion section

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import * as myFunctions from './modules/functions.js';
 import $ from 'jquery';
 import 'slick-carousel';
+import { Fancybox } from "@fancyapps/ui";
 
 myFunctions.isWebp();
 $('.top-slider__inner').slick({
